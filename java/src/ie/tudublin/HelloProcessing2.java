fb7f63fc31fb348f4972e0bbc04ba84b0de186a2
Catriona <catriona.renaghan@gmail.com>
2021-02-03 16:09:38 +0000

    Drawing eye

--- java/src/ie/tudublin/HelloProcessing2.java
+++ java/src/ie/tudublin/HelloProcessing2.java
@@ -16,8 +16,17 @@ public class HelloProcessing2 extends PApplet
 	
 	public void draw()
 	{	
-		background(0);
+		background(255, 0, 0);
 		stroke(255);
-		line(10, 10, 100, 100);
+
+		noStroke();
+		fill(255, 255, 0);
+		ellipse(250, 300, 400, 400);
+		fill(0, 255, 255);
+		triangle(250, 50, 50, 450, 450, 450);
+		fill(180, 200, 200);
+		ellipse(250, 250, 190, 100);
+		fill(0);
+		ellipse(250, 250, 70, 70);
 	}
 }

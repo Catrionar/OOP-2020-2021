955c04b9475cd638bd5993085c47a9d9e937657e
Catriona <catriona.renaghan@gmail.com>
2021-03-22 13:56:19 +0000

    Rotate and move player and add second player

--- java/src/ie/tudublin/YASC.java
+++ java/src/ie/tudublin/YASC.java
@@ -5,7 +5,7 @@ import processing.core.PApplet;
 public class YASC extends PApplet {
     boolean[] keys = new boolean[1024];
 
-    Player p;
+    Player p, p1;
 
     public void settings() {
         size(500, 500);
@@ -13,6 +13,7 @@ public class YASC extends PApplet {
 
     public void setup() {
         p = new Player(this, width/2, height/2);
+        p1 = new Player(this, 100, 100);
     }
 
     public void draw() {
@@ -20,6 +21,9 @@ public class YASC extends PApplet {
         stroke(255);
         p.update();
         p.render();
+
+        p1.update();
+        p1.render();
     }
 
     boolean checkKey(int k) {
